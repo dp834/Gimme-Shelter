a1686e1494765d2f9242e515e2ebe9cfbbe992dc
Damien Prieur <dap789@live.com>
2019-09-06 22:51:53 -0400

    django: renamed project and moved to src folder

--- src/gimme-shelter-django/app/wsgi.py
+++ src/gimme-shelter-django/app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pennapps project.
+WSGI config for gimme-shelter-django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pennapps.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gimmie-shelter-django.settings')
 
 application = get_wsgi_application()
--- src/gimme-shelter-django/manage.py
+++ src/gimme-shelter-django/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pennapps.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gimme-shelter-django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
