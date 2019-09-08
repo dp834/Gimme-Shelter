e4907930c6754842cb44c6000aeedbac1d695a11
Damien Prieur <dap789@live.com>
2019-09-08 00:31:40 -0400

    flask_main: added endpoints for getting db info

--- src/Flask-Server/flask_server_main.py
+++ src/Flask-Server/flask_server_main.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask, request,render_template
 from twilio.twiml.messaging_response import MessagingResponse
+import mysql.connector
 
 
 app = Flask(__name__)
+
+mycursor = None
 
 
 @app.route('/')
@@ -21,7 +24,36 @@ def receiesms():
 
     return str(resp)
 
+@app.route('/update_count', methods=['GET', 'POST'])
+def update_count():
+    table = request.values.get("type", None)
+    name = request.values.get('name', None)
+    new_count = request.values.get('new_count', None)
+    if(None in [table, name, new_count]):
+        resp = MessagingResponse()
+        resp.message("Invalid data")
+        return str(resp)
 
+    mycursor.execute("UPDATE {table} SET curr_count={new_count} WHERE GIMIE_SHELTER.{table}.name = {name};".format(table=table, new_count=new_count, name=name))
+    return ""
+
+@app.route('/get_names', methods=['GET', 'POST'])
+def get_names():
+    table = request.values.get("type", None)
+    if(None in [table]):
+        resp = MessagingResponse()
+        resp.message("Invalid data")
+        return str(resp)
+
+    mycursor.execute("SELECT name FROM GIMMIE_SHELTER.{table};".format(table=table))
+    return ""
+
+@app.route('/get_tables', methods=['GET', 'POST'])
+def get_tables():
+    mycursor.execute('SELECT table_name FROM information_schema.tables WHERE table_schema = \'GIMME_SHELTER\';')
+    return list(mycursor)
+
+    
 @app.route('/<string:page_name>/')
 def render_static(page_name):
     if os.path.exists('/home/gimmeshelter/Flask-Server/templates/%s.html' % page_name):
@@ -33,3 +65,12 @@ def render_static(page_name):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+    mydb = mysql.connector.connect(
+        host="34.67.115.190",
+        user="root",
+        passwd="password123",
+        database="GIMMIE_SHELTER"
+    )
+
+    mycursor = mydb.cursor()
+
